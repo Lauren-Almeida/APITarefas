9f6fbade9c42a6b4070a59b994cad6894c74fb1d
Lauren-Almeida <128049276+Lauren-Almeida@users.noreply.github.com>
2023-06-09 14:41:41 -0300

    Finalização Minimal API
    Curso Web API ASP .NET Core Essencial (.NET 6)
    Seção 4 : 54-55

--- APITarefas/Program.cs
+++ APITarefas/Program.cs
@@ -24,15 +24,44 @@ app.MapGet("frases", async () =>
     );
 
 app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
-app.MapPost("/tarefas",async (Tarefa tarefa,AppDbContext db) =>
+
+app.MapGet("/tarefas/{id}", async (int id, AppDbContext db) =>
+    await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+
+app.MapGet("/tarefas/concluida", async (AppDbContext db) =>
+                                 await db.Tarefas.Where(t => t.IsConcluída).ToListAsync());
+
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
 {
     db.Tarefas.Add(tarefa);
     await db.SaveChangesAsync();
     return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
-    
+});
 
-}
-        );
+app.MapPut("/tarefas/{id}", async (int id, Tarefa inputTarefa, AppDbContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if (tarefa is null) return Results.NotFound();
+
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.IsConcluída = inputTarefa.IsConcluída;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+
+});
+
+app.MapDelete("/tarefas/{id}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+        return Results.Ok(tarefa);
+    }
+    return Results.NotFound();
+});
+
+
 
 app.Run();
 
