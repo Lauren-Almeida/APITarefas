57bbf1c1ce04cad0abda4635e6d27f2ff0ebfd35
Lauren-Almeida <128049276+Lauren-Almeida@users.noreply.github.com>
2023-06-09 13:25:45 -0300

    Criação APITarefas
    Seção 4 : 51-53

---
+++ APITarefas/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("TarefasDB"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("frases", async () =>
+    await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    );
+
+app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
+app.MapPost("/tarefas",async (Tarefa tarefa,AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+    
+
+}
+        );
+
+app.Run();
+
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluída { get; set; }
+
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+
+}
